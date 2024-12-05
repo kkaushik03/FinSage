9bee27b0fa5e4c08e75b8ecc9aea317bcd1f567a
kkaushik03 <kkaushik@csu.fullerton.edu>
2024-12-04 21:35:30 -0800

    Delete finsage directory

--- finsage/finsage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from financial.views import parse_document  # Adjust 'financial' to match your app name
-
-urlpatterns = [
-    path('parse-document/', parse_document, name='parse_document'),
-]
