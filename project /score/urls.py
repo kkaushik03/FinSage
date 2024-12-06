7e1d24b12b66b6a8d329476c7ab7cfae5c24a492
kkaushik03 <kkaushik@csu.fullerton.edu>
2024-12-05 22:43:55 -0800

    Delete project  directory

--- project /score/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Homepage view
-    path('process_input/', views.process_input, name='process_input'),  # Process input
-]
